d31faa42d16ed8c3a37620fe248545ab3b10dfc6
Ryan Lemon <ryanalemon@gmail.com>
2021-11-15 22:25:27 -0800

    First commit post-projects. Redo color palette, welcome, about, skill, and footer sections

---
+++ script.js
@@ -0,0 +1,3 @@
+$("#CS").on("click", () => $("#CS").attr("src", "images/CS Gif.gif"));
+$("#BizOps").on("click", () => $("#BizOps").attr("src", "images/BizOps Gif.gif"));
+$("#TEACH").on("click", () => $("#TEACH").attr("src", "images/TEACH Gif.gif"));
